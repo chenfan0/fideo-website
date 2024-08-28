7210e3c8fe740e52d09d0a7621792fa5f27e0efe
chen fan <1490040662@qq.com>
2024-08-28 19:28:03 +0800

    refacotr: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,40 @@
-import createMiddleware from 'next-intl/middleware';
-import { NextRequest, NextResponse } from 'next/server';
- 
+import createMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
+
 const intlMiddleware = createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'cn'],
- 
+  locales: ["en", "cn"],
+
   // Used when no locale matches
-  defaultLocale: 'cn'
+  defaultLocale: "cn",
 });
- 
+
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(cn|en)/:path*']
+  matcher: ["/", "/(cn|en)/:path*"],
 };
 export default function middleware(request: NextRequest) {
-  // 这里需要处理其他参数，像?q=123
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/cn', request.url));
+  const { pathname, search } = request.nextUrl;
+  console.log(pathname, search, '=================');
+  if (
+    pathname === "/" ||
+    (!pathname.startsWith("/cn") && !pathname.startsWith("/en"))
+  ) {
+    // 获取用户的首选语言
+    const language =
+      request.headers.get("accept-language")?.split(",")[0].split("-")[0] ||
+      "en";
+
+    // 确定重定向的语言前缀
+    const langPrefix = language === "zh" ? "/cn" : "/en";
+
+    // 构建新的 URL
+    const newUrl = `${langPrefix}${pathname === "/" ? "" : pathname}${search}`;
+
+    // 重定向到新的 URL
+    return NextResponse.redirect(new URL(newUrl, request.url));
   }
+
+  // 对于已经有语言前缀的路径,使用 intlMiddleware
   return intlMiddleware(request);
 }
