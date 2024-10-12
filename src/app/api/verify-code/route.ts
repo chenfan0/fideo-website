fb14a1b4205f7626558a8a2ca04b3cc2f4b792bb
chen fan <1490040662@qq.com>
2024-10-13 02:11:58 +0800

    fix: verify-code

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,17 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { getCollection } from "@/app/db/mongo";
+import redis from "@/app/redis/redis";
+import dayjs from "dayjs";
 
 
 export async function GET(request: NextRequest) {
+  // 这个接口是给 web.fideo.site 用来校验的，所以这里状态码是 404
 
   const searchParams = request.nextUrl.searchParams;
   const code = searchParams.get("code");
 
-  const collection = await getCollection("code");
-  const items = await collection.findOne({ code });
+  if (!code) {
+    return NextResponse.json({}, { status: 404 });
+  }
 
-  if (!items) {
+  let alreadyExist = await redis.get(`code:${code}`).catch(() => null)
+
+  if (!alreadyExist) {
+    const collection = await getCollection("code");
+    alreadyExist = await collection.findOne({ code })
+
+    if (alreadyExist) {
+      const expireTime = (alreadyExist as any).expireTime
+
+      if (expireTime && expireTime > dayjs().valueOf()) {
+        alreadyExist = true
+      } else {
+        collection.deleteOne({ code })
+        redis.del(`code:${code}`)
+        alreadyExist = false
+      }
+    }
+  }
+
+  if (!alreadyExist) {
     return NextResponse.json({}, { status: 404 });
   }
 
